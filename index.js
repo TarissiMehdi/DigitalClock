b084a3fc56f91e702efd76e7886855d73f26de50
Trickster <Mehdi.Tarissi@emsi-edu.ma>
2025-10-31 15:48:48 +0100

    Real time clock using meridium system (AM/PM)

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-// Random Password Generator 
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridium = hours>=12 ? "PM" : "AM";
+    hours = hours %12 || 12;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
 
-function generate(){
-    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numbers ="0123456789";
-    const symbols ="&-_@$!§?<>";
+    const timeString = `${hours}:${minutes}:${seconds} ${meridium}`;
 
-    const passwordLength = 12;
-    const includeLowerCase = document.getElementById("allowLower");
-    const includeUpperCase = document.getElementById("allowUpper");
-    const includeNumbers = document.getElementById("allowNumbers");
-    const includeSymbols = document.getElementById("allowSymbols");
-    const passwordInsert = document.getElementById("passwordGen");
-
-    let allowedChars ="";
-    let password = "";
-
-    allowedChars+= includeLowerCase.checked ? lowercaseChars : "";
-    allowedChars+= includeUpperCase.checked ? uppercaseChars : "";
-    allowedChars+= includeNumbers.checked ? numbers : "";
-    allowedChars+= includeSymbols.checked ? symbols : "";
-
-    if(passwordLength<=0) passwordInsert.textContent= "(length should at least be 1)";
-    if(allowedChars.length===0) passwordInsert.textContent= "(at least one set of characters should be selected)";
-
-    for (let i =0;i<passwordLength;i++){
-        const randomIndex = Math.floor(Math.random()*allowedChars.length);
-        password+=allowedChars[randomIndex];
-    }
-    passwordInsert.textContent= `Your generated password is ${password}`;
+    document.getElementById("clock").textContent= timeString;
 }
 
+updateClock();
 
-
-
-
-
-
-
+setInterval(updateClock,1000);
